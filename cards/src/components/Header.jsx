ab240e7cae3a9b0150977b325c47a78c7e3e8974
Hilal Turhan <88663001+hilaltrhaan@users.noreply.github.com>
2022-06-17 18:47:31 +0300

    finish cards

---
+++ cards/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <h1>Personel Cards</h1>
+      
+    </div>
+  )
+}
+
+export default Header
+
